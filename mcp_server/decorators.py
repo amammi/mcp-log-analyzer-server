20361dc1c16d0726ebea78ff700e172969b8eec3
amammi <andrea.mammi@live.it>
2025-07-07 22:29:51 +0200

    feat: added McpServerConfigSetting model + refactoring and adding logger for exceptions in mcp server fix: fixes bug in log_tool_call decorator and fixes api_key and base_url none in agent analyze method after deleting .env file dependency

---
+++ mcp_server/decorators.py
@@ -0,0 +1,14 @@
+from functools import wraps
+
+from mcp.server.fastmcp.utilities.logging import get_logger
+
+logger = get_logger(__name__)
+
+def log_tool_call(tool_fn):
+    @wraps(tool_fn)
+    def wrapper(*args, **kwargs):
+        logger.info(f"[TOOL] Chiamata al tool: {tool_fn.__name__} con args: {args}, kwargs: {kwargs}")
+        result = tool_fn(*args, **kwargs)
+        logger.info(f"[TOOL] Result della chiamata al tool: {result}")
+        return result
+    return wrapper
--- webapp/client_webapp/admin.py
+++ webapp/client_webapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ProviderModel, SelectionParam
+from .models import ProviderModel, SelectionParam, McpServerConfig
+
 
 # Register your models here.
 
@@ -13,4 +14,10 @@ class SelectionParamSetting(admin.ModelAdmin):
         """Di questo ModelAdmin è permessa una sola istanza."""
         return not SelectionParam.objects.exists()
 
-    pass
+
+@admin.register(McpServerConfig)
+class McpServerConfigSetting(admin.ModelAdmin):
+    # TODO support for multi mcp servers
+    def has_add_permission(self, request):
+        """Di questo ModelAdmin è permessa una sola istanza."""
+        return not McpServerConfig.objects.exists()
