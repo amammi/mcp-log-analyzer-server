4dd57038b601926e3126921bf3b1b09908f1d67d
amammi <andrea.mammi@live.it>
2025-07-09 20:17:32 +0200

    fix: fixes wrong admin path in urls of django client_webapp

--- webapp/client_webapp/urls.py
+++ webapp/client_webapp/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", views.log_analyzer_view, name="index"),
     path('api/analyze/', views.analyze_logs_api, name='analyze_logs'),
     path('api/containers/', views.get_active_containers, name='get_containers'),
