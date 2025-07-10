8ea9ff8e2e047992d4ce7e8bed96d63a82dd457b
amammi <andrea.mammi@live.it>
2025-07-10 14:16:41 +0200

    feat: first version of admin panel

--- webapp/client_webapp/admin.py
+++ webapp/client_webapp/admin.py
@@ -4,20 +4,34 @@ from .models import ProviderModel, SelectionParam, McpServerConfig
 
 # Register your models here.
 
-@admin.register(ProviderModel)
+class LogAnalyzerAdminSite(admin.AdminSite):
+    site_header = "AI Log Analyzer"
+    site_title = "AILA Admin Panel"
+    index_title = "AI Log Analyzer Admin Panel"
+    site_url = "/client"
+    index_template = "admin/index.html"
+    app_index_template = "admin/app_index.html"
+
+
+admin_site = LogAnalyzerAdminSite(name="AI Log Analyzer")
+
+
 class ProviderModelSetting(admin.ModelAdmin):
     pass
 
-@admin.register(SelectionParam)
 class SelectionParamSetting(admin.ModelAdmin):
     def has_add_permission(self, request):
         """Di questo ModelAdmin è permessa una sola istanza."""
         return not SelectionParam.objects.exists()
 
 
-@admin.register(McpServerConfig)
 class McpServerConfigSetting(admin.ModelAdmin):
     # TODO support for multi mcp servers
     def has_add_permission(self, request):
         """Di questo ModelAdmin è permessa una sola istanza."""
         return not McpServerConfig.objects.exists()
+
+
+admin_site.register(ProviderModel, ProviderModelSetting)
+admin_site.register(SelectionParam, SelectionParamSetting)
+admin_site.register(McpServerConfig, McpServerConfigSetting)
--- webapp/client_webapp/apps.py
+++ webapp/client_webapp/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ClientWebappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'client_webapp'
+    verbose_name = "Frontend Configuration"
